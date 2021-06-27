1287f2dc24aca367da197e951389ea4fb8ce07ac
joona95 <joona95@naver.com>
2021-06-28 08:24:14 +0900

    refactor : User 생성자에서 객체의 초기화만 수행하도록 수정

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,18 +1,8 @@
 public class User {
     private GameSelections selection;
 
-    public User(int selection) {
-        switch(selection) {
-            case 0:
-                this.selection = GameSelections.SCISSOR;
-                break;
-            case 1:
-                this.selection = GameSelections.ROCK;
-                break;
-            case 2:
-                this.selection = GameSelections.PAPER;
-                break;
-        }
+    public User(GameSelections selection) {
+        this.selection = selection;
     }
 
     public GameSelections getSelection(){
