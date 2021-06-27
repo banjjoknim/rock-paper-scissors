7873c1eb0f05fe6aa15b4b6028f482cd9c98033b
joona95 <joona95@naver.com>
2021-06-28 08:59:02 +0900

    refactor : 만들어진 User와 Computer 객체로 Game 객체 생성하도록 수정

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -7,9 +7,9 @@ public class Game {
     private User user;
     private Computer computer;
 
-    private Game(User user) {
+    private Game(User user, Computer computer) {
         this.user = user;
-        this.computer = new Computer();
+        this.computer = computer;
     }
 
     private GameResults gameStart() {
@@ -27,8 +27,8 @@ public class Game {
     }
 
     private void printResult(GameResults result) {
-        System.out.println("컴퓨터의 선택지 : " + computer.getSelectionValue());
-        System.out.println("유저의 선택지 : " + user.getSelectionValue());
+        System.out.println("컴퓨터의 선택지 : " + computer.getSelection().getValue());
+        System.out.println("유저의 선택지 : " + user.getSelection().getValue());
         System.out.println("결과 : " + result.value());
     }
 
@@ -36,8 +36,11 @@ public class Game {
         System.out.println("가위 바위 보 게임을 시작합니다.");
         System.out.println("선택지를 입력해주세요. 가위 = 0, 바위 = 1, 보 = 2");
         Scanner sc = new Scanner(System.in);
-        User user = new User(sc.nextInt());
-        Game game = new Game(user);
+        GameSelections userSelection = GameSelections.getGameSelection(sc.nextInt());
+        GameSelections computerSelection = GameSelections.getGameSelection(GameSelections.makeSelection());
+        User user = new User(userSelection);
+        Computer computer = new Computer(computerSelection);
+        Game game = new Game(user, computer);
         GameResults result = game.gameStart();
         game.printResult(result);
     }
