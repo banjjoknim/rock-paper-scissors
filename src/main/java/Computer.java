6f3f8f50335690c3ed7192ac96bd14f246b98fdc
joona95 <joona95@naver.com>
2021-06-28 08:22:41 +0900

    refactor : Computer 생성자에서 객체의 초기화만 수행하도록 수정

--- src/main/java/Computer.java
+++ src/main/java/Computer.java
@@ -1,19 +1,8 @@
 public class Computer {
     private GameSelections selection;
 
-    public Computer(){
-        int selection = (int) (Math.random() * 3);
-        switch(selection) {
-            case 0:
-                this.selection = GameSelections.SCISSOR;
-                break;
-            case 1:
-                this.selection = GameSelections.ROCK;
-                break;
-            case 2:
-                this.selection = GameSelections.PAPER;
-                break;
-        }
+    public Computer(GameSelections selection){
+        this.selection = selection;
     }
     public GameSelections getSelection(){
         return selection;
