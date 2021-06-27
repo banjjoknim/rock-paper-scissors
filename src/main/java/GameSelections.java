20e526d7b1b528e3ff2a278b1fdece62692e7e95
joona95 <joona95@naver.com>
2021-06-28 08:27:41 +0900

    refactor : GameSelections에서 선택 숫자도 저장하도록 수정

--- src/main/java/GameSelections.java
+++ src/main/java/GameSelections.java
@@ -1,9 +1,13 @@
 public enum GameSelections {
-    SCISSOR("가위"), ROCK("바위"), PAPER("보");
+    SCISSOR(0, "가위"),
+    ROCK(1, "바위"),
+    PAPER(2, "보");
 
+    private final int selectNumber;
     private final String selections;
 
-    GameSelections(String selections) {
+    GameSelections(int selectNumber, String selections) {
+        this.selectNumber = selectNumber;
         this.selections = selections;
     }
 
