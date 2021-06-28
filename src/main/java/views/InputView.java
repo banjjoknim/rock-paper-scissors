3dbe59ad27638f6aa475f1835f4c763908e64ed1
joona95 <joona95@naver.com>
2021-06-28 09:38:20 +0900

    refactor : InputView에서 input 관리하기 위해 입력 함수 추가

--- src/main/java/views/InputView.java
+++ src/main/java/views/InputView.java
@@ -1,4 +1,11 @@
 package views;
 
+import java.util.Scanner;
+
 public class InputView {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static int getInput() {
+        return scanner.nextInt();
+    }
 }
