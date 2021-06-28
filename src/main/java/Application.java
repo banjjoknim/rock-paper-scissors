d15c4432031c8869985cc523ec4334a01502808a
joona95 <joona95@naver.com>
2021-06-28 09:40:44 +0900

    refactor : Game 클래스가 controller 역할을 하도록 변경

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,11 @@
-public class Application {
+import controller.GameController;
+import enums.GameResults;
+import enums.GameSelections;
+import models.Computer;
+import models.User;
 
+public class Application {
+    public static void main(String[] args) {
+        GameController.gameRun();
+    }
 }
--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -4,36 +4,21 @@ import enums.GameResults;
 import enums.GameSelections;
 import models.Computer;
 import models.User;
+import views.InputView;
+import views.OutputView;
 
 import java.util.Scanner;
 
 public class GameController {
-
-    private User user;
-    private Computer computer;
-
-    private GameController(User user, Computer computer) {
-        this.user = user;
-        this.computer = computer;
-    }
-
-    private void printResult(GameResults result) {
-        System.out.println("컴퓨터의 선택지 : " + computer.getSelection().getValue());
-        System.out.println("유저의 선택지 : " + user.getSelection().getValue());
-        System.out.println("결과 : " + result.getValue());
-    }
-
-    public static void main(String[] args) {
-        System.out.println("가위 바위 보 게임을 시작합니다.");
-        System.out.println("선택지를 입력해주세요. 가위 = 0, 바위 = 1, 보 = 2");
-        Scanner sc = new Scanner(System.in);
-        GameSelections userSelection = GameSelections.getGameSelection(sc.nextInt());
-        GameSelections computerSelection = GameSelections.getGameSelection(GameSelections.makeSelection());
-        User user = new User(userSelection);
-        Computer computer = new Computer(computerSelection);
-        GameController game = new GameController(user, computer);
+    public static void gameRun() {
+        OutputView.printGameStart();
+        int userSelect = InputView.getInput();
+        GameSelections userSelection = GameSelections.getGameSelection(userSelect);
+        int computerSelect = GameSelections.makeSelection();
+        GameSelections computerSelection = GameSelections.getGameSelection(computerSelect);
         GameResults result = GameSelections.getGameResult(userSelection, computerSelection);
-        game.printResult(result);
+        OutputView.printGameSelections(userSelection, computerSelection);
+        OutputView.printGameResult(result);
     }
 }
 
