ab7172af4359e05a5c55bfbede195cf00836470e
joona95 <joona95@naver.com>
2021-06-28 23:23:01 +0900

    refactor : GameService를 통해서 GameResults 가져올 수 있도록 수정

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -2,6 +2,8 @@ package controller;
 
 import enums.GameResults;
 import enums.GameSelections;
+import models.Game;
+import models.GameService;
 import views.InputView;
 import views.OutputView;
 
@@ -9,11 +11,11 @@ public class GameController {
     public static void gameRun() {
         OutputView.printGameStart();
         int userSelect = InputView.getInput();
-        GameSelections userSelection = GameSelections.getGameSelection(userSelect);
         int computerSelect = GameSelections.makeSelection();
-        GameSelections computerSelection = GameSelections.getGameSelection(computerSelect);
-        GameResults result = GameSelections.getGameResult(userSelection, computerSelection);
-        OutputView.printGameSelections(userSelection, computerSelection);
+        Game game = GameService.makeGame(userSelect, computerSelect);
+        OutputView.printComputerSelections(game.getComputer());
+        OutputView.printUserSelections(game.getUser());
+        GameResults result = GameService.gameStart(game);
         OutputView.printGameResult(result);
     }
 }
